060eed9372e8988ea9f19a02342da168e18e6131
Nicole D <nnardella@gmail.com>
2019-01-02 16:06:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,6 +100,11 @@ class Student
     end
 
     def self.first_student_in_grade_10
+      sql = <<-SQL
+        SELECT * FROM students
+        WHERE grade = 10
+        LIMIT 1
+      SQL
 
     end
 
