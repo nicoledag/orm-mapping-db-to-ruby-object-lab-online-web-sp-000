894fe13faf29a27326cc9d0954bebac8a6c6321c
Nicole D <nnardella@gmail.com>
2019-01-02 15:35:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,9 @@ class Student
   def self.new_from_db(row)
     binding.pry
     new_student = self.new   #same as saying Student.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
 
   end
 
