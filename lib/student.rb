fac9ee3fa3dd3cdec217f59a0ba3738ef90fc7dd
Nicole D <nnardella@gmail.com>
2019-01-02 15:54:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,8 +80,10 @@ class Student
       SELECT * FROM students
       WHERE grade < 12
     SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
+    DB[:conn].execute(sql)
+
+    # DB[:conn].execute(sql).map do |row|
+    #   self.new_from_db(row)
     end
 
   end
