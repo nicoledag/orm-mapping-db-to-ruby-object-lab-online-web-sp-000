da84746378a4ad18040f12c0cf5b6afb13012cc2
Nicole D <nnardella@gmail.com>
2019-01-02 16:01:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,7 +90,7 @@ class Student
 
     def self.first_X_students_in_grade_10(X)
       sql = <<-SQL
-        SELECT COUNT()
+        SELECT COUNT(X)
         FROM students
         WHERE grade = 10
       SQL
