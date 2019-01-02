178f41e9d2c0ec18f1317cf7f382018d73cbd0a9
Nicole D <nnardella@gmail.com>
2019-01-02 15:59:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,7 +89,14 @@ class Student
     end
 
     def self.first_X_students_in_grade_10(X)
-      
+      sql = <<-SQL
+        SELECT * FROM students
+        WHERE grade = 10
+      SQL
+
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
 
 
     end
